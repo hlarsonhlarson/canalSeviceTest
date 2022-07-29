415d4b57493d877fcee7d7ffedc857f36a20a18d
hlarsonhlarson <aa-badalov1@mail.ru>
2022-07-29 19:27:05 +0300

    change name to main

--- quikstart.py
+++
@@ -1,6 +0,0 @@
-from google_api_worker import google_creds_getter, get_info_spreadsheets
-
-if __name__ == '__main__':
-    creds = google_creds_getter()
-    x = get_info_spreadsheets(creds)
-    print(x)
